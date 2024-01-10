c428c61d74ec1ab86c13821dcf2e9fb922bc1067
lixiang <1938623228@qq.com>
2024-01-10 17:54:47 +0800

    feat(member): 创建member数据库表，并使用mybatis查询数据库

---
+++ member/src/main/java/com/study/train/member/mapper/MemberMapper.java
@@ -0,0 +1,6 @@
+package com.study.train.member.mapper;
+
+public interface MemberMapper {
+
+    int count();
+}
---
+++ member/src/main/java/com/study/train/member/service/MemberService.java
@@ -0,0 +1,15 @@
+package com.study.train.member.service;
+
+import com.study.train.member.mapper.MemberMapper;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+    @Resource
+    private MemberMapper memberMapper;
+
+    public int count(){
+        return memberMapper.count();
+    }
+}

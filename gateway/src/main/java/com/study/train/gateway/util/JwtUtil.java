bff21f4d00eb7d502bf3b423b5031e9a80a1c3d8
lixiang <1938623228@qq.com>
2024-01-12 18:42:21 +0800

    feat(web): 通过axios在前端增加统一的请求的拦截

--- gateway/src/main/java/com/study/train/gateway/util/JwtUtil.java
+++ gateway/src/main/java/com/study/train/gateway/util/JwtUtil.java
@@ -39,11 +39,16 @@ public class JwtUtil {
     }
 
     public static boolean validate(String token) {
-        JWT jwt = JWTUtil.parseToken(token).setKey(key.getBytes());
-        // validate包含了verify
-        boolean validate = jwt.validate(0);
-        LOG.info("JWT token校验结果：{}", validate);
-        return validate;
+        try {
+            JWT jwt = JWTUtil.parseToken(token).setKey(key.getBytes());
+            // validate包含了verify
+            boolean validate = jwt.validate(0);
+            LOG.info("JWT token校验结果：{}", validate);
+            return validate;
+        }catch (Exception e){
+            LOG.error("JWT token校验异常",e);
+            return false;
+        }
     }
 
     public static JSONObject getJSONObject(String token) {

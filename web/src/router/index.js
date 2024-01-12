1a60c6cb0fdd0bf526de11f2e0b2ad33cd748c99
lixiang <1938623228@qq.com>
2024-01-12 23:35:23 +0800

    feat(member): 增加乘车人信息表和持久层代码

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -15,7 +15,10 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/main.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/main.vue'),
+    meta: {
+      loginRequire: true
+    }
   }
 ]
 
@@ -23,5 +26,26 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+// 路由登录拦截
+router.beforeEach((to, from, next) => {
+  // 要不要对meta.loginRequire属性做监控拦截
+  if (to.matched.some(function (item) {
+    console.log(item, "是否需要登录校验：", item.meta.loginRequire || false);
+    return item.meta.loginRequire
+  })) {
+    const _member = store.state.member;
+    console.log("页面登录校验开始：", _member);
+    if (!_member.token) {
+      console.log("用户未登录或登录超时！");
+      notification.error({ description: "未登录或登录超时" });
+      next('/login');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
